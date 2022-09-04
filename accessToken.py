9c7aa66bade097e746185703313c517bc5e9b046
1486308032 <50775291+1486308032@users.noreply.github.com>
2022-09-04 14:43:49 +0800

    Create accessToken.py

---
+++ accessToken.py
@@ -0,0 +1,20 @@
+# 多人模式accessToken的填写模板
+# 注意格式，包括{}自身以及内部的内容，不能漏掉任何符号
+# 用户名自己取，accessToken要保证正确
+"""此处为注释，仅供参考
+data = {
+    "用户1": "accessToken",
+    "用户2": "accessToken",
+    "用户3": "accessToken"
+}
+"""
+
+# 下列代码将生效
+data = {
+    "用户1": "accessToken1",
+    "用户2": "accessToken2",
+    "用户3": "accessToken3"
+}
+
+if __name__ == '__main__':
+    print(data)
